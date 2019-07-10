f701ecbf2dc885f6be39735501fe1e2aadfbe81e
Tom North <tnorth93@gmail.com>
2019-07-09 19:54:44 -0700

    removed unnecessary lines from index.js and App.js(x)

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <div>
+        <h1>Rick and Morty</h1>
+        <p>Pick your favourite episodes</p>
+      </div>
+    </React.Fragment>
+  );
+}
