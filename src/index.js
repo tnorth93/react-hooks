74b074894d4dfc3529d9729d22e3927768de94ad
Tom North <tnorth93@gmail.com>
2019-07-31 22:59:30 -0700

    updated index.js for navigation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { StoreProvider } from './Store';
 import './index.css';
+import HomePage from './HomePage';
+import { Router } from '@reach/router';
 
-ReactDOM.render(<StoreProvider>
-    <App /></StoreProvider>, document.getElementById('root'));
+ReactDOM.render(
+<StoreProvider>
+    <Router>
+        <App path='/'>
+            <HomePage path='/'/>
+        </App>
+    </Router>
+</StoreProvider>, document.getElementById('root'));
