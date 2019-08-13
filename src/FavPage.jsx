09c92fbb2cc94b6dac0de5e760ab205a9ae560f7
Tom North <tnorth93@gmail.com>
2019-08-12 22:28:08 -0700

    added props and export for favpage function

--- src/FavPage.jsx
+++ src/FavPage.jsx
@@ -2,3 +2,28 @@ import React from 'react';
 import { Store } from './Store';
 
 const EpisodesList = React.lazy(() => import('./EpisodesList'));
+
+export default function FavPage() {
+    const
+   { state, dispatch } = React.useContext(Store);
+   
+   const props = {
+      
+  episodes: state.favourites,
+      
+  toggleFavAction: toggleFavAction,
+      
+  favourites: state.favourites
+    
+  };
+
+  return (
+    <React.Suspense 
+fallback={<div>Loading...</div>}>
+      <div 
+className='episode-layout'>
+        <EpisodesList {...props} />
+      </div>
+    </React.Suspense>
+  );
+}
